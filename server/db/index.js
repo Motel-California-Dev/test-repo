bf13d0478503b0f9fd730c29423641bf1ea67e3b
kylemazza <kyleamazza@gmail.com>
2019-02-02 11:51:20 -0800

    Changed server/db/index.js to include POSTGRES_PASSWORD field necessary for login

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DB
 });
